27ba1874640a4f13df3e0a8bdedc23bf1a2d7007
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-08 08:47:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Hello guys")
+
+menu = ['Introduction','Environnement_d_un_Projet Smart Building','Visu Audit Energie + Deploiement Iot','Visu Solutions Iot + Plan Comptage','Visu Conso Energies(Elec/Gaz/Eau)',"Bilan Conso Energies(Elec/Gaz/Eau)"," Axes d'amélioration identifiées",'Visu Suivi des KPI Conso(Elec/Gaz/Eau)','Visu objectifs Réduction Conso Energies','Info + contacts utiles sites']
+choice=st.sidebar.selectbox("Menu",menu)
+   
