6b8df98d18164e7e59d36a946d006c79b47fa1c4
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-16 18:44:03 +0200

    com60

--- app.py
+++ app.py
@@ -699,10 +699,10 @@ def main():
     st.sidebar.title("Présentation Projet APEGG")
 
     # Ajouter les éléments du menu
-    menu_selection = st.sidebar.radio("Sélectionnez une option",("Menu_1","Menu_2","Menu _3","Menu_4","Menu_5","Menu_6"))
+    menu_selection = st.sidebar.radio("Sélectionnez une option",("1_Conception","Menu_2","Menu _3","Menu_4","Menu_5","Menu_6"))
 
     # Afficher le contenu en fonction de la sélection du menu
-    if menu_selection == "Menu_1":
+    if menu_selection == "1_Conception":
         menu1()
     elif menu_selection == "Menu_2":
         menu2()
