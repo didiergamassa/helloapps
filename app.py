5e039fd067507a05f048184ce5cb2def60f5395f
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-17 18:47:29 +0200

    com60

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def main():
             
             
             
-            st.markdown(''' ##### Notre Vision: ''')
+            st.markdown(''' ##### Notre Vision ''')
             st.markdown(''' ######  Une Vision enracinée dans l'Engagement: ''')
             st.markdown(''' Chez L'APEGG, nous nous engageons à perpétuer l'héritage du Dr. Alain Pensé GAMASSA en étant des agents de changement pour la transition écologique. Notre vision est de créer un monde où l'innovation technologique et l'ingénierie sont mises au service de la préservation de notre planète et de l'amélioration de la qualité de vie pour tous. ''')            
         
