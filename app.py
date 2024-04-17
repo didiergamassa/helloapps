f1213e80e353b3281b06a741be8a4972b47ee201
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-17 18:54:57 +0200

    com60

--- app.py
+++ app.py
@@ -141,8 +141,8 @@ def main():
             st.markdown(''' Environnement : Nous nous engageons à protéger et à restaurer l'environnement en proposant des solutions d'ingénierie environnementale intégrées, visant à réduire la pollution, à préserver les ressources naturelles et à promouvoir la durabilité         ''')
             
             st.markdown(''' Maintenance : Enfin, notre expertise en maintenance permet à nos clients de garantir le bon fonctionnement de leurs infrastructures et équipements, en minimisant les temps d'arrêt, en optimisant les coûts et en prolongeant la durée de vie des actifs.''')
-            
-            
+            st.markdown(''' ##### Notre Engagement ''')
+            st.markdown(''' Chez l'APEEG,nous croyons en un avenir ou ma technologie et l'innovation sont mises au service de la durabilité et de la prospérité pour tous.Nous nous engageons à travailler en partenariat avec nos clients pour créer un impact positif sur la société et l'environnement,et à oeuvrer  ensemble pour une transition écologique globale reussie.''')
             
             
             st.markdown(''' ##### Notre Vision ''')
