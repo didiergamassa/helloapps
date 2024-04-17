63212813aa2500fbc35633da82e211bf28b3e2f7
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-17 16:39:37 +0200

    com30

--- app.py
+++ app.py
@@ -774,6 +774,9 @@ def main():
             st.markdown(''' ##### 2. Base de données de test intégré dans la plate forme Orange Live Objects  :''')
             st.markdown(''' ##### 3. Compteur Electrique Communicant Linky  :             ''')
             st.markdown(''' ##### 3. Capteur IoT de test  :  A commander chez Fludia ou Watteco  ''')
+            st.markdown(''' ##### 4. Catologue des Capteur Iot Dataprint      ''')
+            url34="https://www.dataprint.fr/support/Catalogue-IoT-DataPrint.pdf    "
+            st.markdown(f'<a href="{url34}" target="_blank">{"Cliquez ici pour visiter"+"www.Catalogue_Capeur_Iot_Dataprint "}</a>', unsafe_allow_html=True)
             st.markdown(''' ##### 4. Passerelle Iot de test  :  A commander chez Dataprint      ''')
             st.markdown(''' ##### 5. Notice de configuration de la passerelle Multitech avant exploitation réseau :  ''')    
             url33="https://multitech.com/wp-content/uploads/s000727-mPower-Edge-Intelligence-Conduit-AEP-software-guide.pdf"
